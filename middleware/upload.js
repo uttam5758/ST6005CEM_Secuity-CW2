2ed5461e550856b032cab03348ea599be5cfdd8b
Uttam Sapkota <uttamsapkota.danger@gmail.com>
2026-01-28 22:50:43 +0545

    Manage uploads js

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,12 +4,12 @@ const path =  require("path")
 const storage =  multer.diskStorage({
     destination : (req,file,cb)=>{
         cb(null,'./uploads')
-    },
-    filename : (req,file,cb) =>{
-        let ext =  path.extname(file.originalname)
-        cb(null,`${file.fieldname}-${Date.now()}${ext}`)
-    }
-})
+//     },
+//     filename : (req,file,cb) =>{
+//         let ext =  path.extname(file.originalname)
+//         cb(null,`${file.fieldname}-${Date.now()}${ext}`)
+//     }
+// })
 const imagefileFilter =  (req,file,cb)=>{
     if(!file.originalname.match(/\.(jpg|jpeg|png|gif|Svg|jfif)$/)){
         return cb(new Error("You can upload only image files"),false)
